eedfd818ef3204807cc652ab7ae7b54c5447a305
Henrique Soares <henrique.de.f.soares@gmail.com>
2022-07-15 16:11:23 -0400

    Merge tokens into one class

--- compiler/tokens.py
+++ compiler/tokens.py
@@ -2,76 +2,49 @@ from enum import Enum
 
 
 class Tag(Enum):
-    INTEGER = 1
-    REAL = 2
-    ADD = 3
-    SUB = 4
-    MUL = 5
-    DIV = 6
-    EXP = 7
-    PAR_BEGIN = 8
-    PAR_END = 9
-    EULER_BEGIN = 10
-    EULER_END = 11
+    NUM = 0
+    ADD = 1
+    SUB = 2
+    MUL = 3
+    DIV = 4
+    EXP = 5
+    OPEN_PAR = 6
+    CLOSE_PAR = 7
+    OPEN_BRACKET = 8
+    CLOSE_BRACKET = 9
+    EULER = 10
+    END = 11
 
 
 class Token:
-    def __init__(self, tag):
+    def __init__(self, tag, buffer=''):
         self.tag = tag
 
+        if tag == Tag.NUM:
+            self.symbol = 'num'
+            self.lexval = float(buffer)
+        if tag == Tag.ADD:
+            self.symbol = '+'
+        if tag == Tag.SUB:
+            self.symbol = '-'
+        if tag == Tag.MUL:
+            self.symbol = '*'
+        if tag == Tag.DIV:
+            self.symbol = '/'
+        if tag == Tag.EXP:
+            self.symbol = '^'
+        if tag == Tag.OPEN_PAR:
+            self.symbol = '('
+        if tag == Tag.CLOSE_PAR:
+            self.symbol = ')'
+        if tag == Tag.OPEN_BRACKET:
+            self.symbol = '['
+        if tag == Tag.CLOSE_BRACKET:
+            self.symbol = ']'
+        if tag == Tag.EULER:
+            self.symbol = 'exp'
+        if tag == Tag.END:
+            self.symbol = '$'
 
-class Integer(Token):
-    def __init__(self, num):
-        super().__init__(Tag.INTEGER)
-        self.num = num
-
-
-class Real(Token):
-    def __init__(self, num):
-        super().__init__(Tag.REAL)
-        self.num = num
-
-
-class Add(Token):
-    def __init__(self):
-        super().__init__(Tag.ADD)
-
-
-class Sub(Token):
-    def __init__(self):
-        super().__init__(Tag.SUB)
-
-
-class Mul(Token):
-    def __init__(self):
-        super().__init__(Tag.MUL)
-
-
-class Div(Token):
-    def __init__(self):
-        super().__init__(Tag.DIV)
-
-
-class Exp(Token):
-    def __init__(self):
-        super().__init__(Tag.EXP)
-
-
-class ParBegin(Token):
-    def __init__(self):
-        super().__init__(Tag.PAR_BEGIN)
-
-
-class ParEnd(Token):
-    def __init__(self):
-        super().__init__(Tag.PAR_END)
-
-
-class EulerBegin(Token):
-    def __init__(self):
-        super().__init__(Tag.EULER_BEGIN)
-
-
-class EulerEnd(Token):
-    def __init__(self):
-        super().__init__(Tag.EULER_END)
+    def get_symbol(self):
+        return self.symbol
