fcbb277b5aa4198c5b6c74f25a6351debd0f6217
Henrique Soares <henrique.de.f.soares@gmail.com>
2022-07-18 16:12:13 -0400

    Add syntax analysis and execution to main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#from parser import parser
+from compiler.parser import Parser
+from compiler.utils import print_tree
 import argparse
 
+
 def main():
-    
-    # Prase arguments
-    parser = argparse.ArgumentParser(description='Compiler')
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='A compiler for simple math expressions')
     parser.add_argument('path',
                         type=str,
                         help='Path to the source code file')
@@ -13,16 +16,16 @@ def main():
     # Read input file
     with open(args.path) as file:
         source_code = file.read()
-        
-    # Syntax analisys
-    parser = Parser(source_code)
-    
-    parse_tree = parser.parse()
-    
-    # Semantic analisys
 
-    # Intemediate code generator
+    # Analyse syntax and compute answer
+    for line in source_code.split('\n'):
+        print(line)
+        parser = Parser(line)
+        parser.parse_tree()
+        # print('Syntax tree:')
+        # print_tree(parser.tree)
+        # print('\n\n')
+        parser.execute()
 
-    # Write output file
-    
+
 main()
